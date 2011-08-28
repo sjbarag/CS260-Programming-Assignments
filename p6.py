64ade0b35057930dcefe01da940e7ff18e7f6a05
Sean Barag sjb89@drexel.edu <Sean Barag sjb89@drexel.edu>
2011-08-28 19:22:42 -0400

    Dijkstra distance calculations complete.  Should still add path storage.

--- p6.py
+++ p6.py
@@ -1,48 +1,47 @@
-def DIJKSTRA(C):
+def DIJKSTRA2() :
 	"""
 	computes the cost of the shortest paths from vertex 0 to every vertex of a
 	directed graph
 
 	param C		n x n array of costs
 	"""
-	global n
-	global p
-	V = range(0, n)
+	n = 6
 	S = [0]
-	D = []
-	for i in range(n) :
-		D.append(None)
-	#D = []*n
-	for i in range(1, n) :
+	V = range(n)
+	D = [0, 0, 0, 0, 0, 0]
+	C = [ [ 0,  4,  1,  5,  8, 10], \
+		  [50,  0, 50, 50, 50, 50], \
+	  	  [50,  2,  0, 50, 50, 50], \
+	  	  [50, 50, 50,  0,  2, 50], \
+		  [50, 50, 50, 50,  0,  1], \
+		  [50, 50, 50, 50, 50,  0] ]
+	for i in range(1,n) :
 		D[i] = C[0][i]
-		p[i] = 0
 	for i in range(n-1) :
-		Ds = []
-		vMinusS = [j for j in V if j not in S]
-		for k in vMinusS:		# thanks, StackOverflow!
-			Ds.append(D[k])							# as with DFS, there was no
-													#   requirement to not use
-													#   the built-in list types.
-													#   I am using them merely
-													#   for convenience.
-		w = min(Ds)
-		S.append(w)
+		vMinusS = [e for e in V if e not in S]
+		w = min([ D[f] for f in vMinusS ])
+		w = D.index(w)
+		if w not in S :
+			S.append(w)
+
+		vMinusS = [e for e in V if e not in S]
 		for v in vMinusS :
-			if D[v] > D[w] + C[w][v] :
-				D[v] = D[w] + C[w][v]
-				p[v] = w
+			D[v] = min( D[v], D[w] + C[w][v] )
+	return D
+
+
 
 
 global n
 n = 6
 
 # initialize!
-c = [ [0, 4, 1, 5, 8, 10], \
-	  [0, 0, 0, 0, 0,  0], \
-	  [0, 2, 0, 0, 0,  0], \
-	  [0, 0, 0, 0, 2,  0], \
-	  [0, 0, 0, 0, 0,  1], \
-	  [0, 0, 0, 0, 0,  0] ]
+c = [ [ 0,  4,  1,  5,  8, 10], \
+	  [50,  0, 50, 50, 50, 50], \
+	  [50,  2,  0, 50, 50, 50], \
+	  [50, 50, 50,  0,  2, 50], \
+	  [50, 50, 50, 50,  0,  1], \
+	  [50, 50, 50, 50, 50,  0] ]
 
 # these aren't very Pythonic, but [[0]*n]*n resulted in each row being a copy
 #   of the first one, including later assignments.
@@ -53,8 +52,9 @@ a = [ [0, 0, 0, 0, 0,  0], \
 	  [0, 0, 0, 0, 0,  0], \
 	  [0, 0, 0, 0, 0,  0] ]
 
-p = [0, 0, 0, 0, 0, 0]
 
-print p
-DIJKSTRA(c)
-print p
+#print DIJKSTRA2(c)
+print "Final distances: "
+dist = DIJKSTRA2()
+for i in range(n) :
+	print "0 ->", i, ":", dist[i]
