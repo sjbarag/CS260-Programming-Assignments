ae6b4066df60e0fa7359b18a6ce2ab107c3b4fa3
Sean Barag sjb89@drexel.edu <Sean Barag sjb89@drexel.edu>
2011-08-28 15:23:57 -0400

    Depth-first search is complete!

--- p9.py
+++ p9.py
@@ -1,5 +1,8 @@
 def DFS(v) :
-	"""performs depth-first search"""
+	"""
+	performs depth-first search, and prints the current node
+	@param v		the current node in the search
+	""" # I finally switched to Pythonic documentation syntax.  Not too sure if I like it...
 	global mark
 	global L
 	print LETTERS[v],
@@ -11,8 +14,7 @@ def DFS(v) :
 NUMNODES = 6
 LETTERS = ['a', 'b', 'c', 'd', 'e', 'f']
 mark = [False]*NUMNODES
-# NOTE: A..F has be remapped to 0..5 to make things simpler.  This could be
-#   done with letters, but everything would need to be hashed first
+# NOTE: A..F has be remapped to 0..5 to make things simpler for the creation of the adjacency list.  These numbers are translated back into letters before printing.
 # adjacency list, taken from figure 6.38 (page 226).  I am using Python's
 #   built-in list type here for simplicity, as there does not appear to be a
 #   requirement to use my own implementation.
